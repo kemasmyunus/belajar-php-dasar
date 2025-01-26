c72e9c2ec2bd6d63c33d6c3d4c0821f46b1141ae
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-26 19:43:07 +0800

    belajar-php-dasar-hari1-commit3

---
+++ 2-tipe-data-integer.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Decimal : ";
+var_dump(2081);
+echo "HexaDecimal : ";
+var_dump(0xADF1);
+echo "Octal : ";
+var_dump(0o2134);
+echo "Biner : ";
+var_dump(0b010110);
+echo "Decimal : ";
+var_dump(1_201_395);
