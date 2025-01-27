ae401d9a0947d4d6d5fa36b5d865efd207896d0d
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-27 21:24:23 +0800

    belajar-javascript-dasar-hari3-commit4

---
+++ 3-tipe-data-boolean.php
@@ -0,0 +1,8 @@
+<?php
+$salah = false;
+$benar = true;
+echo "ini adalah contoh nilai dari tipe data boolean";
+echo "ini nilai salah = ";
+var_dump($salah);
+echo "ini nilai benar = ";
+var_dump($benar);
