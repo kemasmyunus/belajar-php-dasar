0f0ab17428b671f99b1d6efbfae5dd91fa3f9ab8
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-27 21:17:03 +0800

    belajar-javascript-dasar-hari3-commit2

--- 2-tipe-data-integer.php
+++ 2-tipe-data-integer.php
@@ -1,31 +1,31 @@
 <?php
 
 echo "Decimal : ";
-var_dump(1234);
+var_dump(234);
 
 echo "Octal : ";
-var_dump(01234);
+var_dump(0o12345);
 
 echo "Hexadecimal : ";
-var_dump(0x1A);
+var_dump(0x11FA);
 
 echo "Binary : ";
-var_dump(0b111111);
+var_dump(0b10011);
 
 echo "Underscore in number : ";
-var_dump(1_241_241_241);
+var_dump(3_412_345);
 
 echo "Floating Point : ";
-var_dump(1.234);
+var_dump(1.134);
 
 echo "Floating Point dengan E Notation : (1.7 x 1000) : ";
-var_dump(1.7e3);
+var_dump(1.3e3);
 
 echo "Floating Point dengan E Notation minus : (1.7 x 0.001) : ";
-var_dump(1.7e-3);
+var_dump(1.4e-3);
 
 echo "Underscore di floating point : ";
-var_dump(1_123.123);
+var_dump(4_214.23);
 
 echo "Integer Overflow : ";
-var_dump(9223372036854775808);
+var_dump(9223372036854775809);
