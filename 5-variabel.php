de5b9667c6791c2f45656abdfe7fbefe35da3206
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-28 19:28:25 +0800

    update: hari 3, variabel

---
+++ 5-variabel.php
@@ -0,0 +1,7 @@
+<?php
+
+// variabel dalam php ditulis dengan diawali tanda $ lalu nama variabelnya, contohnya
+$nama = "Kemas M. Yunus";
+$kelas = "XI TKJ";
+
+echo "Halo, nama saya adalah $nama, saya kelas $kelas";
