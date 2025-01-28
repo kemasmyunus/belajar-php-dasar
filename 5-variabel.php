4d4cf3cc11afe69bc739da927be46c71a8c17579
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-28 19:32:42 +0800

    update: hari 3, variabel variabel :v

--- 5-variabel.php
+++ 5-variabel.php
@@ -5,3 +5,14 @@ $nama = "Kemas M. Yunus";
 $kelas = "XI TKJ";
 
 echo "Halo, nama saya adalah $nama, saya kelas $kelas";
+
+$dataSiswa = "Elaina";
+$$dataSiswa = "Elaina";
+
+echo "data siswa : ";
+echo $dataSiswa;
+echo "\n";
+
+echo "Elaina : ";
+echo $Elaina;
+echo "\n";
