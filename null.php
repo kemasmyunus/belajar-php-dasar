fed7bd2f79c53bbe1a36d9125c1cf9f459e71043
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-29 18:38:07 +0800

    (update) memperbarui file null

--- null.php
+++ null.php
@@ -1,8 +1,17 @@
 <?php
-$nama = "elaina";
-$elaina = [
-    "cinta" => null
+$char = [
+    "elaina" => ["cinta" => "noah"],
+    "mikasa" => ["cinta" => null], //tydak happy ending
+    "emilia" => ["cinta" => "subaru"],
+    "shiina" => ["cinta" => "sorata"],
+    "reina" => ["cinta" => "taki"]
 ];
 
-$cintaDetector = $elaina["cinta"] ?? "yunus";
-echo "cinta dari $nama adalah $cintaDetector\n";
+foreach ($char as $nama => $detail) {
+    $cinta = $detail["cinta"] ?? "yntkts";
+    if($cinta != "yunus"){
+        echo "Cinta dari $nama adalah yunus\n";
+    }else{
+        echo "Cinta dari $nama adalah $cinta\n";
+    }
+}
