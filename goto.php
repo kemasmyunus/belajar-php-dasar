5d5de45144c0bf718ae6b163fb6a69ef78772709
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-30 13:06:58 +0800

    (update) belajar goto

---
+++ goto.php
@@ -0,0 +1,15 @@
+<?php
+
+
+episode1:
+$waifu = "Yuki Suou";
+echo "waifu saya adalah $waifu\n";
+goto episode3;
+
+episode2:
+$waifu = "Alisa Mikhailovna Kujou";
+echo "waifu saya adalah $waifu\n";
+
+episode3:
+$waifu = "Maria Mikhailovna Kujou";
+echo "waifu saya adalah $waifu\n";
